aa91e5a8814c7a68d557a12e20d29ba640d9d0f7
AKHIL <akhileshshakampally@gmail.com>
2022-06-30 17:23:31 +0530

    table and documents list showing in table done

--- src/app/checklist/checklist.component.ts
+++ src/app/checklist/checklist.component.ts
@@ -1,6 +1,7 @@
 import { nullSafeIsEquivalent } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { FileUploadService } from '../file-upload.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-checklist',
@@ -8,6 +9,8 @@ import { FileUploadService } from '../file-upload.service';
   styleUrls: ['./checklist.component.css']
 })
 export class ChecklistComponent implements OnInit {
+
+
 
   constructor(private fileUploadService: FileUploadService) { }
 
@@ -25,10 +28,22 @@ export class ChecklistComponent implements OnInit {
   qualification:boolean=false;
   any_id:boolean=false;
   document_type: string="";
+  emp_id:any=0;
+  pipe = new DatePipe('en-US');
+  updatedDate= this.pipe.transform(Date.now(), 'dd/MM/yyyy');
+  a:any;
 
-onselect(value:string){
+  docList=[
+    {"name":"fake","type":"fake","size":0,"updatedDate":this.updatedDate,"docType":this.document_type}
+  ];
+  
+  addDocument(a:any){
+      this.docList.push(a);
+      console.log(this.docList);
+  }
+  onSelect(value:string){
   this.document_type=value;
-}
+  }
   onWorkProof(){
     this.work_proof=!this.work_proof;
     console.log(this.work_proof);
@@ -59,21 +74,43 @@ onselect(value:string){
 }
 
 // OnClick of button Upload
-onUpload() {
-    this.loading = !this.loading;
-    console.log(this.file);
-    this.fileUploadService.upload(this.file).subscribe(
-        (event: any) => {
-            if (typeof (event) === 'object') {
 
-                // Short link via api response
-                this.shortLink = event.link;
-
-                this.loading = false; // Flag variable 
-            }
-        }
-    );
+getFileDetails (event:any) {
+  for (var i = 0; i < event.target.files.length; i++) { 
+    var name = event.target.files[i].name;
+    var type = event.target.files[i].type;
+    var size = event.target.files[i].size;
+    var modifiedDate = event.target.files[i].lastModifiedDate;
+    
+    console.log ('Name: ' + name + "\n" + 
+      'Type: ' + type + "\n" +
+      'Last-Modified-Date: ' + modifiedDate + "\n" +
+      'Size: ' + Math.round(size / 1024) + " KB");
+  }
+  var a={"name":name,"type":type,"size":size,"updatedDate":this.updatedDate,"docType":this.document_type};
+  this.a=a;
 }
+onUpload() {
+  /*this.loading = !this.loading;
+  console.log(this.file);
+  this.fileUploadService.upload(this.file).subscribe(
+      (event: any) => {
+          if (typeof (event) === 'object') {
 
+              // Short link via api response
+              this.shortLink = event.link;
+
+              this.loading = false; // Flag variable 
+          }
+      }
+  );*/
+if (this.a!=null && this.document_type!=""){
+ this.addDocument(this.a);
+}
+else{
+  alert("please select a document and document type")
+}
+  
+}
 
 }
